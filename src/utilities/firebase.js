a3d8001aa95ac03a28a8c9f6ca2f0bab5c09e905
Ellie Li <ellieli@Ellies-MacBook-Pro-10.local>
2024-11-11 21:20:16 -0600

    firebase

---
+++ src/utilities/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCUa4NVc7Qjss_fqotQ1nAUEKsb-mD2GcY",
+  authDomain: "communifridge.firebaseapp.com",
+  projectId: "communifridge",
+  storageBucket: "communifridge.firebasestorage.app",
+  messagingSenderId: "218603385279",
+  appId: "1:218603385279:web:e9f0eb4c9e0fbca676710a"
+};
+
+
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
