6f8dedfad88dfc568ff42ba83e1d5a6aa2039e5c
Ellie Li <ellieli@Ellies-MacBook-Pro-10.local>
2024-11-17 11:33:17 -0600

    home page button

--- src/components/FridgePageTemp.jsx
+++ src/components/FridgePageTemp.jsx
@@ -1,10 +1,13 @@
 import { useDbData } from "../utilities/firebase";
 import { useParams } from "react-router-dom";
 import FridgeCard from "./FridgeCard";
+import { useNavigate } from 'react-router-dom';
 
 const FridgePageTemp = () => {
     const { fridgeId } = useParams();
     const [items, error] = useDbData(`/fridges/${fridgeId}/items`); 
+    const navigate = useNavigate();
+
 
     console.log(`/fridges/${fridgeId}/items`);
 
@@ -12,10 +15,25 @@ const FridgePageTemp = () => {
     if (items === undefined) return <h1>Loading data...</h1>;
     if (!items) return <h1>No items found</h1>;
 
+    const goToHome = (id) => {
+        navigate(`/`);
+    }
+
     return (
         <div>
-            <h1>Fridge Items</h1>
-            {Object.entries(items).map(([itemId, item]) => <FridgeCard key={itemId} fridgeId={fridgeId} itemId={itemId} item={item}/>)}
+            <div>
+                <h1>Fridge Items</h1>
+                {Object.entries(items).map(([itemId, item]) => <FridgeCard key={itemId} fridgeId={fridgeId} itemId={itemId} item={item}/>)}
+            </div>
+
+            <div className="mt-4 flex flex-col space-y-4 items-center">
+                <button
+                    onClick={() => goToHome()}
+                    className="bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600 transition"
+                >
+                    Home Page
+                </button>        
+            </div>
         </div>
     );
     
