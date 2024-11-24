8d74edb242f02c915688ca842c5b016f6f23ac7e
patrickchen8 <patrick.chen0459@gmail.com>
2024-11-24 02:02:10 -0600

    Added Time Verification

--- src/pages/FridgePageTemp.jsx
+++ src/pages/FridgePageTemp.jsx
@@ -1,4 +1,4 @@
-import { useDbData } from "../utilities/firebase";
+import { useDbData, useDbUpdate } from "../utilities/firebase";
 import { useParams } from "react-router-dom";
 import { useNavigate } from 'react-router-dom';
 import { useState } from "react";
@@ -10,6 +10,7 @@ const FridgePageTemp = () => {
     const { fridgeId } = useParams();
     const navigate = useNavigate();
     const [fridge, error] = useDbData(`/fridges/${fridgeId}`); 
+    const [update, result] = useDbUpdate(`/fridges/${fridgeId}`);
     const [isOpen, setIsOpen] = useState(false);
 
     if (error) return <h1>Error loading data: {error.toString()}</h1>;
@@ -17,11 +18,25 @@ const FridgePageTemp = () => {
     if (!fridge.items) return <h1>No items found</h1>;
 
 
+    const handleVerifcation = () => {
+        const now = new Date();
+        const currentDate = now.toLocaleDateString();
+        const currentTime = now.toLocaleTimeString();
+        update(
+            {
+                verificationDate: currentDate, 
+                verificationTime: currentTime
+            }
+        )
+    }
+
+
     return (
         <div>
             <BackButton />
             <div>
-                <h1 className="text-3xl font-semibold text-center text-blue-800 mb-6">{fridge.displayName} Fridge</h1>
+                <h1 className="text-3xl font-semibold text-center text-blue-800 mb-2">{fridge.displayName} Fridge</h1>
+                <h3 className="text-lg text-center mb-6">{`Items Last Verified at ${fridge.verificationDate} ${fridge.verificationTime}`}</h3>
                 {Object.entries(fridge.items).map(([itemId, item]) => <FridgeCard key={itemId} fridgeId={fridgeId} itemId={itemId} item={item}/>)}
             </div>
 
@@ -38,6 +53,11 @@ const FridgePageTemp = () => {
                 <button onClick={() => navigate(`/fridge/${fridgeId}/request/create`)}
                         className="border-1 border-blue-600 rounded-md bg-blue-300 p-3 text-white hover:bg-blue-400">
                     Make a Request
+                </button>
+
+                <button onClick={handleVerifcation}
+                        className="border-1 border-red-600 rounded-md bg-red-400 p-3 text-white hover:bg-red-500">
+                    Verify Items 
                 </button>
 
             </div>
